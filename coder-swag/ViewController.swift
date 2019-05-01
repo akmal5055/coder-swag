b397ffbb7058c4e99c98bfc79724022d9b79156e
akmal5055 <akmal_5055@icloud.com>
2019-05-01 17:53:08 -0400

    initial commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Akmal Sanginov on 5/1/19.
+//  Copyright © 2019 Sanginov5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
